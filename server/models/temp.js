84f0e0e0b97a2ba03d77775f9f78d327097a27fe
isaacgr <isaacgrowell@gmail.com>
2019-01-12 12:09:28 -0500

    Remove version key from schema

--- server/models/temp.js
+++ server/models/temp.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
+const tempSchema = new mongoose.Schema(
+  {
+    location: { type: String, required: true },
+    values: [
+      {
+        temp: { type: Number, required: true },
+        humid: { type: Number, required: true },
+        temp_f: { type: Number, default: null },
+        createdAt: { type: String, default: null }
+      }
+    ]
+  },
+  { versionKey: false }
+);
 
-const Temp = mongoose.model("Dashboard", {
-  location: { type: String, required: true },
-  values: [
-    {
-      temp: { type: Number, required: true },
-      humid: { type: Number, required: true },
-      temp_f: { type: Number, default: null },
-      createdAt: { type: String, default: null }
-    }
-  ]
-});
+const Temp = mongoose.model("Dashboard", tempSchema);
 
 module.exports = { Temp };
