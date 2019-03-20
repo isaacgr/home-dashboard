cae53796c611440b03bc6016c76cd8ff7233449a
isaacgr <isaacgrowell@gmail.com>
2019-03-20 07:29:28 -0400

    Render card content with dashboard component Pass state with context api Fetch all card data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { GooeyMenu } from "../components/NavBar";
-import { Loader } from "../components/Loader";
-import Card from "../components/Card";
-// import Footer from "../components/Footer";
+import Dashboard from "../components/Dashboard";
+import { fetchData } from "../functions/fetchData";
+
+const AppContext = React.createContext({});
+
 class HomePage extends Component {
   state = {
     temperatureData: {
-      data: undefined
-    }
+      data: []
+    },
+    cardData: []
   };
   componentDidMount() {
     fetch("/api/temp/", {
@@ -19,51 +22,28 @@ class HomePage extends Component {
         return response.json();
       })
       .then(json => {
-        console.log(json);
         this.setState({
           temperatureData: {
             data: json["data"]
           }
         });
       });
+    fetchData().then(result => {
+      this.setState({ cardData: result });
+    });
   }
   render() {
     return (
       <section className="data">
         <GooeyMenu />
         <div className="container">
-          {this.state.temperatureData["data"] ? (
-            this.state.temperatureData["data"].map(dataset => (
-              <Card
-                title={"Comfort"}
-                classes={"u-bg-color-red-dark"}
-                contentTitle={dataset.location}
-                icon={"fas fa-bed"}
-                values={[
-                  {
-                    content: `${dataset.values.temp} \xB0C`,
-                    icon: "fas fa-thermometer-half"
-                  },
-                  {
-                    content: `${dataset.values.humid} %`,
-                    icon: "fas fa-tint"
-                  }
-                ]}
-                footerContent={{
-                  title: "Last Updated",
-                  content: dataset.values.createdAt
-                }}
-              />
-            ))
-          ) : (
-            <div className="container">
-              <Loader />
-            </div>
-          )}
+          <AppContext.Provider value={this.state}>
+            <Dashboard />
+          </AppContext.Provider>
         </div>
       </section>
     );
   }
 }
 
-export default HomePage;
+export { AppContext, HomePage as default };
