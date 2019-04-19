6e6f9b8169c33bcb43913c1271b9ccfcbebd93ac
isaacgr <isaacgrowell@gmail.com>
2019-04-19 12:14:00 -0400

    Add login and registration tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -15,6 +15,7 @@ beforeAll(done => {
 });
 
 afterAll(done => {
+  mongoose.connection.db.dropDatabase();
   mongoose.connection.close();
   server.close(done);
 });
@@ -97,12 +98,16 @@ describe("GET /api/temp", () => {
         expect(response.body["data"][1]["values"]).toMatchObject({
           temp: 30,
           temp_f: 78,
-          humid: 60
+          humid: 60,
+          _id: expect.any(String),
+          createdAt: expect.any(String)
         });
         expect(response.body["data"][0]["values"]).toMatchObject({
           temp: 23,
           temp_f: 72,
-          humid: 30
+          humid: 30,
+          _id: expect.any(String),
+          createdAt: expect.any(String)
         });
       })
       .end(done);
@@ -127,6 +132,46 @@ describe("GET /api/temp/all", () => {
         expect(response.body.length).toBe(2);
         expect(response.body[0]["values"].length).toBe(1);
         expect(response.body[1]["values"].length).toBe(1);
+      })
+      .end(done);
+  });
+});
+
+describe("POST /register", () => {
+  it("should return success message", done => {
+    const body = {
+      username: "irowell",
+      email: "irowell@evertz.com",
+      password: "testicles"
+    };
+    request(app)
+      .post("/register")
+      .send(body)
+      .expect(201)
+      .expect(response => {
+        expect.objectContaining({
+          message: "user added successfully"
+        });
+      })
+      .end(done);
+  });
+});
+
+describe("POST /login", () => {
+  it("should return success message", done => {
+    const body = {
+      username: "irowell",
+      email: "irowell@evertz.com",
+      password: "testicles"
+    };
+    request(app)
+      .post("/login")
+      .send(body)
+      .expect(200)
+      .expect(response => {
+        expect.objectContaining({
+          token: expect.any(String)
+        });
       })
       .end(done);
   });
